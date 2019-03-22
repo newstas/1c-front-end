a1d3fd0dc473f48721b35133c75ddfbee893d411
newstas <45275123+newstas@users.noreply.github.com>
2019-03-22 14:34:53 +0500

    Update script_otladka.js

--- example_14/script_otladka.js
+++ example_14/script_otladka.js
@@ -1,1 +1,24 @@
-script_otladka.js
+var field = document.getElementById('field');
+
+function isPime(n) {
+    for(var i = 2; i < n; ++i) {
+        /* если n делится на какое-либо
+          из чисел от 2 до n - 1, то оно составное */
+        if(n % i ==0) {
+            return false;
+        }
+    }
+    /* если n ни на что не разделилось, то оно простое */
+    return true;
+}
+
+field.addEventListener("keydown", function(event) {
+    var namber, i;
+    /* если нажата клавиша Enter */
+    if(event.which == 13) {
+        /* точка останова */
+        debugger;
+        number = Number(field.value);
+        alert(isPrime(number) ? "Простое!" "Составное");
+    }
+});

9da3de2770d31d680fe2af0a88ffb980ab732a4d
newstas <45275123+newstas@users.noreply.github.com>
2019-03-22 12:16:51 +0500

    Переименовал файл
    Переименовал файл в основной для урока

--- example/script.js
+++ example/script.js
@@ -1,16 +1,51 @@
 var canvas = document.getElementById("sandbox"),
-context = canvas.getContext("2d"),
-line = new Path2D();
+    context = canvas.getContext("2d");
 
-line.moveTo(0, 0);
-line.lineTo(300, 300);
+function drawLine(angle, a, b, style, width) {
+    var R = 300 / 2, pX, pY, qX, qY;
+    pX = Math.cos(angle) * R;
+    pY = -Math.sin(angle) * R;
+    qX = a * pX; qY = a * pY;
+    pX *= b; pY *= b;
+    pX += R; pY += R;
+    qX += R; qY+= R;
+    var line;
+    line = new Path2D();
+    line.moveTo(pX, pY);
+    line.lineTo(qX, qY);
+    context.strokeStyle = style;
+    context.lineWidth = width;
+    context.stroke(line);
+}
 
-context.lineWidth = 5;
-context.stroke(line);
+function drawWatch() {
+    var R = 300 / 2, d, angle, pX, pY, qX, qY;
+    var division, circle;
+    context.clearRect(0, 0, 2 * R, 2 * R);
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    circle = new Path2D();
+    circle.arc(R, R, R, 0, 2 * Math.PI);
+    context.stroke(circle);
+    for(d = 0; d < 60; ++d) {
+        angle = (d / 60) * (2 * Math.PI);
+        drawLine(angle, 0.9, 1, d % 5 == 0 ? "black" : "rgba(0, 0, 0.5)", 1);
+    }
 
-context.fillStyle = "red";
-context.fillRect(0, 0, 200, 200);
+    var date = new Date(),
+        hours = date.getHours(),
+        minutes = date.getMinutes(),
+        seconds = date.getSeconds();
+    var secondsAngle = (seconds / 60) * (2 * Math.PI),
+        minutesAngle = (minutes / 60) * (2 * Math.PI),
+        hoursAngle = ((hours % 12) / 12) * (2 * Math.PI);
+    secondsAngle = Math.PI / 2 - secondsAngle;
+    minutesAngle = Math.PI / 2 - minutesAngle;
+    hoursAngle = Math.PI / 2 - hoursAngle;
+    drawLine(secondsAngle, 0, 0.95, "red", 2);
+    drawLine(minutesAngle, 0, 0.75, "black", 5);
+    drawLine(hoursAngle, 0, 0.5, "black", 10);
+    setTimeout(drawWatch, 1000);
+}
 
-/* красный: 0%, зеленый: 100%, синий: 0%, прозрачный: 50% */
-context.fillStyle = "rgba(0, 225, 0, 0.5)";
-context.fillRect(100, 100, 300, 300);
+drawWatch();

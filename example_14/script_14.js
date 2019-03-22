057c43b3872ecd2eb003133e1e142eaa1db96dc7
newstas <45275123+newstas@users.noreply.github.com>
2019-03-22 17:04:16 +0500

    Update script_14.js

--- example_14/script_14.js
+++ example_14/script_14.js
@@ -3,3 +3,14 @@ reference = {key: "volue"};
 dictionary == reference
 
 console.log("The Zen of Python:\n\"Beatiful is better than ugly\'");
+
+
+// 2.5 Контекст вызова
+function getCaller() {
+    return this;
+}
+
+var alice = { name: "Alice", fn: getCaller },
+    bob = { name: "Bob", fn: getCaller };
+
+alice.fn()

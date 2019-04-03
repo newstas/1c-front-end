c2782f8c4a709ec9164d12dbed9a47346d76c45e
newstas <45275123+newstas@users.noreply.github.com>
2019-04-04 00:09:50 +0500

    comm28

--- example_15/server/index.js
+++ example_15/server/index.js
@@ -1,2 +1,9 @@
 // index.js
-console.log('hello from Node.js')
+var express = require("express");
+var app = express();
+
+app.get("/", function(request, response) {
+	response.send("Hello? Node.js!");
+});
+
+app.listen(591);

2bbb5621b42dbab348e6921c0268356b66fe1bdc
newstas <45275123+newstas@users.noreply.github.com>
2019-04-04 02:04:26 +0500

    47

--- example_15/server/index.js
+++ example_15/server/index.js
@@ -1,6 +1,9 @@
 // index.js
 var express = require("express");
+    cors = request("cors");
 var app = express();
+
+app.use(cors());
 
 app.get("/", function(request, response) {
 	response.send("Hello? Node.js!");

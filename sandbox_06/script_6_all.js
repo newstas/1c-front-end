f53317d69d22fee0d2cc2d84fd785cabda7be5c1
newstas <45275123+newstas@users.noreply.github.com>
2019-04-13 12:43:13 +0500

    73

---
+++ sandbox_06/script_6_all.js
@@ -0,0 +1,91 @@
+var = duck {
+    name: "Уточка",
+    weight: 10,
+    quack: "Кря!",
+
+
+    checkAvailability: function () {
+        return this.rooms - this.boocked;
+    }
+}
+
+
+var square = document.createElement("div");
+container.appendChild(square);
+
+
+
+
+
+var i;
+for (i = 0; i < 3; i = i + 1) {
+    console.log(i);
+}
+
+
+var i = 1;
+while (i < 3) {
+    console.log(i);
+    i = i + 1;
+}
+
+
+
+
+
+var i = 0;
+while (i < 3) {
+    console.log(i);
+    i = i + 1;
+}
+
+
+var i = 0;
+while (i < 3) {
+    i = i + 1;
+    console.log(i);
+}
+
+
+
+
+function truthy() {
+    console.log("Я истина!");
+    return true;
+}
+
+function falsy() {
+    console.log("Я ложна!");
+    return false;
+}
+
+if (truthy() || falsy()) {
+    console.log("Условие верно!");
+}
+
+
+
+
+function min(a, b, c) {
+    if (a < b && a < c) {
+        return a;
+    } else if (b < a && b < c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+
+var i;
+for (i = 0; i < 3; i = i + 1) {
+    console.log(i);
+}
+
+var duck = {
+    name: "Уточка",
+    weight: 10,
+
+    quack: function () {
+        console.log("Кря!");
+    }
+}

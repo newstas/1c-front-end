fa0df705061743a7d9ffd7d57d87b2297cc875ff
newstas <45275123+newstas@users.noreply.github.com>
2019-04-13 12:49:10 +0500

    82

---
+++ sand_Yn/script_Yn.js
@@ -0,0 +1,17 @@
+let persone = {
+    name: "Jone",
+    age: 25,
+    isMarried: false
+}
+
+// console.log(persone["name"]);
+
+// alert("Hello World!");
+
+/* let answer = confirm("Are you here?");
+console.log(answer) */
+
+console.log(typeof(null));
+
+
+

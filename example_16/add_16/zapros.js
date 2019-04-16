4e85bc3bfdf311b84e592962d84e61225ae40c02
newstas <45275123+newstas@users.noreply.github.com>
2019-04-17 02:55:37 +0500

    79

--- example_16/add_16/zapros.js
+++ example_16/add_16/zapros.js
@@ -1,4 +1,9 @@
 // Запросы для добавление в консоль
+// Добавляем документы в коллекцию
 db.humans.insert({ name: "John", age: 30 })
 //
 db.humans.insert({ name: "Mary", age: 23 })
+
+// Запрос выводящий все домкументов
+var documents = db.humans.find();
+documents.pretty()

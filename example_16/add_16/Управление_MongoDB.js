db4d85080cf04cd575dfc1a332a1f1e68908a15d
newstas <45275123+newstas@users.noreply.github.com>
2019-04-18 02:00:07 +0500

    97

--- example_16/add_16/Управление_MongoDB.js
+++ example_16/add_16/Управление_MongoDB.js
@@ -1,80 +1,87 @@
 // Переходим к папке bin
 
-Set-Location -Path D:\MongoDB\Server\4.0\bin
+    Set-Location D:\MongoDB\Server\4.0\bin
 
 // Переход к папке example_16\mongo
 
-Set-Location -Path F:\Stanislav\Workspace\GitHub\1c-front-end\example_16\mongo
+    Set-Location F:\Stanislav\Workspace\GitHub\1c-front-end\example_16\mongo
 
+// По умолчанию CD в PowerShell является псевдонимом для Set-Location
+
+    sd F:\Stanislav\Workspace\GitHub\1c-front-end\example_16\mongo
+
+    Set-Location F:\Stanislav\Workspace\GitHub\1c-front-end\example_16_form\server
 
 // mkdir - команда для создания новых каталогов (например для базы данных db)
 
-mkdir
+    mkdir
 
 // rm - команда для удаления файлов и папок
 
-rm
+    rm
 
 // Установка модуля mongodb для связи Node.js с базами данных MongoDB
 
-npm install mongodb --save
+    npm install mongodb --save
 
 // Устанвливаем нужную версию MongoDB
 
-npm install mongodb@2.2.33 --save
+    npm install mongodb@2.2.33 --save
 
 // Вывод списка баз данных
 
-show databases
+    show databases
 
-show dbs
+    show dbs
 
 // запустить сервер MongoDB. Сервер представляет приложение mongod, которое находится в папке bin.
 // mongod: сервер баз данных MongoDB. Он обрабатывает запросы, управляет форматом данных и выполняет различные операции в фоновом режиме по управлению базами данных
 
-mongod
+    mongod
 
 // Выбор базы данных
 
-use sanbox
+    use sanbox
 
 // Чтобы проверить какая база данных выбрана в данный момент, выполните
 
-db
+    db
 
 // Просмотр коллекций(таблиц) в выбранной базе данных
 
-show collections
+    show collections
 
-db.getCollectionNames()
+    db.getCollectionNames()
 
 // Получить статистику по текущей базе данных
 
-db.stats()
+    db.stats()
 
 // Добавление данных в коллекцию
 
-insert()
+    insert()
 
 // Добавляет несколько документов
 
-insertMany()
+    insertMany()
 
 // Добавляет один документ
 
-insertOne()
+    insertOne()
 
 //  Смотрим документ в БД
 
-db.humans.find()
+    db.humans.find()
 
 // Чтобы вывести документ в более читабельном виде добавим метод pretty()
 
-db.humans.find().pretty()
+    db.humans.find().pretty()
 
-// Для загузки файла в текущую базу данных применяется функция load(), в которую в качестве параметра передается путь к файлу. В данном случае предполагается, что файл располагается по пути "D:/users.js".
+// Для загузки файла в текущую базу данных применяется функция load(),
+// в которую в качестве параметра передается путь к файлу.
+// В данном случае предполагается, что файл располагается по пути "D:/users.js".
 
-load("D:/users.js")
+    load("D:/users.js")
 
 
-db.sanbox.find().pretty()
+    db.sanbox.find().pretty()
---
+++ example_16/add_16/Управление_npm.js
@@ -0,0 +1,17 @@
+
+
+//Устанавливаем модуль Express
+
+    npm install express --save
+
+// Устанвливаем молудь CORS
+
+    npm install cors --save
+
+// Установка модуля mongodb для связи Node.js с базами данных MongoDB
+
+    npm install mongodb --save
+
+// Устанвливаем нужную версию MongoDB
+
+    npm install mongodb@2.2.33 --save

598a86bf026ed8c6c59d0712e57c3cd913c85d47
newstas <45275123+newstas@users.noreply.github.com>
2019-05-18 06:39:01 +0500

    46

--- example_19_2/database.js
+++ example_19_2/database.js
@@ -3,6 +3,6 @@ module.exports = [
         name: "Фильм",
         year: 1970,
         posterUrl: "https://placehold.it/500",
-        description: "Описание фильма"
+        description: "Описание фильма."
     }
-]
+];
